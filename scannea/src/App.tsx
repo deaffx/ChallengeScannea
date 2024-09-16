fbe49ef45c6bf841d57e3f4fca8e696317ba9bbd
deaffx <thiago10mm@gmail.com>
2024-09-16 17:13:40 -0300

    add routes

--- scannea/src/App.tsx
+++ scannea/src/App.tsx
@@ -1,10 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home.tsx";
+import Contato from "./components/Contato/Contato.tsx";
+import Cadastro from "./components/Cadastro/Cadastro.tsx";
+import Chatbot from "./components/Chatbot/Chatbot.tsx";
+import Login from "./components/Login/Login.tsx";
+import NotFound from "./components/NotFound/NotFound.tsx";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App: React.FC = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/chatbot" element={<Chatbot />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} /> {/* Rota 404 */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
