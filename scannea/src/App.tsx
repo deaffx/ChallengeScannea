4fbf4b89afc0e576a6e286f787c8f071891b4a14
deaffx <thiago10mm@gmail.com>
2024-09-16 17:19:18 -0300

    route home edited

--- scannea/src/App.tsx
+++ scannea/src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
     <Router>
       <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contato" element={<Contato />} />
         <Route path="/cadastro" element={<Cadastro />} />
